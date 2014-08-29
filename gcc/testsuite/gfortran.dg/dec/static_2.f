b4ee9d3c82aa5d1a2d0b8de384163ff43041f13e
Fritz Reese <fritzoreese@gmail.com>
2014-08-29 16:52:11 -0400

    Test for correct semantics of AUTOMATIC with -fno-automatic in static_2.

--- gcc/testsuite/gfortran.dg/dec/static_2.f
+++ gcc/testsuite/gfortran.dg/dec/static_2.f
@@ -7,20 +7,44 @@
       !
       include 'assert.inc'
 
-      function f (x, y)
-        integer f, x, y, a      ! only a can actually be saved
-        integer, automatic :: c ! { dg-warning "marked AUTOMATIC" }
-        c = c + y ! should increment by y every time
-        f = c
+      function f (x)
+      implicit none
+        integer f, x
+        integer, static :: a ! should be SAVEd
+        a = a + x ! should increment by x every time
+        f = a
         return
       endfunction
 
+      recursive subroutine g (x)
       implicit none
+        integer x
+        integer, automatic :: a ! should be automatic (in recursive)
+        a = a + x ! should be set to x every time
+        call assertll ("g%a", a, x)
+      endsubroutine
+
+      subroutine h (x)
+      implicit none
+        integer x
+        integer, automatic :: a ! should be automatic (outside recursive)
+        a = a + x ! should be set to x every time
+        call assertll ("h%a", a, x)
+      endsubroutine
+
       integer f
 
-      ! Should return static value of a; accumulates x
-      call assertll ("f()", f(1,3), 3)
-      call assertll ("f()", f(1,4), 7)
-      call assertll ("f()", f(1,2), 9)
+      ! Should return static value of c; accumulates x
+      call assertll ("f()", f(3), 3)
+      call assertll ("f()", f(4), 7)
+      call assertll ("f()", f(2), 9)
+
+      call g(3)
+      call g(4)
+      call g(2)
+
+      call h(3)
+      call h(4)
+      call h(2)
 
       end
