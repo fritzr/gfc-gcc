e943c7bbca2e567750d20d74c7604554c150dfc1
Fritz Reese <fritzoreese@gmail.com>
2014-12-02 14:20:27 -0500

    Update lazy_ints test to use -flazy-logicals

--- gcc/testsuite/gfortran.dg/dec/lazy_ints.f
+++ gcc/testsuite/gfortran.dg/dec/lazy_ints.f
@@ -1,4 +1,4 @@
-      ! { dg-options "-flazy-types" }
+      ! { dg-options "-flazy-logicals" }
       ! { dg-do run }
       ! Tests the conversions between integers and logicals using
       ! -flazy-types. This helps compatibility with old (albeit faulty) code.
@@ -6,7 +6,7 @@
       subroutine abortis (i, s)
         integer, intent(in) :: i
         character(*), intent(in) :: s
-        write (*, '(a,i2,a,a)') "(ti = ", i, ") ", s
+        write (*, '(a,i2,a,a)') "(int = ", i, ") ", s
         call abort()
       end subroutine
 
@@ -18,46 +18,59 @@
 
       program main
 
-      integer tis(2) /-1,1/
-      integer :: i = 1       ! ti = tis(ind)
-      integer :: ti          ! true int
-      integer :: fi = 0      ! false int
-      logical :: tl = .true. ! true bool
-      logical :: fl          ! false bool
+      integer tis(7) /1,-1, -5,-3,3,5,7/
+      integer fis(7) /0, 2, -6,-4,-2,4,6/
+      integer :: ii = 1            ! [ft]i = [ft]is(ii)
+      integer :: fi, ti            ! false int, true int
+      logical :: tl = .true.       ! true logical
+      logical :: fl = .false.      ! false logical
 
-      if (fi) call aborts ("    false int")
-      if (.not. fi) then
-      else
-        call aborts ("not false int")
-      endif
-
-      if (fl .ne. .false.) call aborts ("false bool .ne.")
-      if (tl .ne. .true.)  call aborts ("true  bool .ne.")
+      ! Test behavior of logicals with eq/ne instead of eqv/neqv
+      if (fl .ne. .false.) call aborts ("false logical .ne.")
+      if (tl .ne. .true.)  call aborts ("true  logical .ne.")
       if (fl .eq. .false.) then
       else
-        call aborts ("false bool .eq.")
+        call aborts ("false logical .eq.")
       endif
       if (tl .eq. .true.) then
       else
-        call aborts ("true  bool .eq.")
+        call aborts ("true  logical .eq.")
       endif
 
-      if (tl .eq. fi) call aborts ("true  bool == false int")
-      if (fl .ne. fi) call aborts ("false bool != false int")
+      ! Test how integers handle as logicals
+      ! For compatibility with Intel and other Fortran compilers, we
+      ! define an integer as true if its lsb is 1, false if it is 0.
+      do while ( ii .le. size(tis) )
+        fi = fis(ii)
+        ti = tis(ii)
 
-      ! Test how non-zero integers handle as logicals
-      ! -1 as true provides Intel compatibility, 1 as true works otherwise
-      do while ( i .le. size(tis) )
-        ti = tis(i)
-        if (.not. ti) call abortis (ti, "not true  int")
+        if (fi) call abortis (fi, "false int")
+        if (.not. fi) then
+        else
+          call abortis (fi, "not false int")
+        endif
+c       In Intel FT, .false. is only .eq. 0
+        if (fi .eq. 0) then
+          if (tl.eq.fi) call abortis (fi, "true  logical ==  false int")
+          if (fl.ne.fi) call abortis (fi, "false logical !=  false int")
+        endif
+        if (tl .eqv. fi) call abortis (fi,"true  logical === false int")
+        if (fl .neqv. fi) call abortis(fi,"false logical !== false int")
+
+        if (.not. ti) call abortis (ti, "not true int")
         if (ti) then
         else
-          call abortis (ti, "true  int")
+          call abortis (ti, "true int")
         endif
-        if (tl .ne. ti) call abortis (ti, "true  bool != true  int")
-        if (fl .eq. ti) call abortis (ti, "false bool == true  int")
+c       In Intel FT, .true. is only .eq. -1
+        if (ti .eq. -1) then
+          if (tl.ne.ti) call abortis (ti,  "true  logical !=  true int")
+          if (fl.eq.ti) call abortis (ti,  "false logical ==  true int")
+        endif
+        if (tl .neqv. ti) call abortis (ti,"true  logical !== true int")
+        if (fl .eqv. ti) call abortis (ti, "false logical === true int")
 
-        i = i + 1
+        ii = ii + 1
       end do
 
       end program
